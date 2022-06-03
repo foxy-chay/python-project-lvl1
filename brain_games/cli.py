797d02f14ed6efec5dbbcc630237a72bb2306675
Andrey Volochay <2025651+volAV@users.noreply.github.com>
2022-06-03 22:12:17 +0200

    Refactor the code to be WPS compliant

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,6 +1,13 @@
+"""Set of functions to be used in comandline."""
+
 import prompt
 
 
 def welcome_user():
+    """Says hello to user.
+
+    Returns:
+        Greeting with user name.
+    """
     name = prompt.string('May I have your name? ')
-    print(f'Hello, {name}!')
+    return 'Hello, {0}!'.format(name)
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
+"""Main script to run brain_games app."""
 
 from brain_games.cli import welcome_user
 
 
 def main():
+    """Beggining of Brain Games app."""
     print('Welcome to the Brain Games!')
-    welcome_user()
+    print(welcome_user())
+
 
 if __name__ == '__main__':
     main()
